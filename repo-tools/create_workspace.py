81cd2279d8dc591d89a127701f7c891ab8d93488
Jeff Verkoeyen <jeff@clutch.engineering>
2025-04-26 10:29:20 -0700

    Tune create workspace.

--- repo-tools/create_workspace.py
+++ repo-tools/create_workspace.py
@@ -13,11 +13,11 @@ def main():
     parser = argparse.ArgumentParser(description='Extract OBD parameter data for the OBDb Explorer')
     parser.add_argument('--org', default='OBDb', help='GitHub organization name')
     parser.add_argument('--workspace', default='workspace', help='Workspace directory for cloning repos')
+    parser.add_argument('--filter-prefix', action='append', help='Filter repositories to only those with the specified prefix (can be used multiple times)')
     args = parser.parse_args()
 
-    clone_repos(args.org, args.workspace)
-
-    print(f"Data extraction complete. The JSON file is ready for use in the React application.")
+    clone_repos(args.org, args.workspace, args.filter_prefix)
 
 if __name__ == '__main__':
     main()
+o1
